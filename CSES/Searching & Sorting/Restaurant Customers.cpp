cdffcf08c088a2f25494b24aba46d11aa69c9ae9
Shahriar Rizvi <44527871+rizvi19@users.noreply.github.com>
2022-09-14 11:59:18 +0600

    Update Restaurant Customers.cpp

--- CSES/Searching & Sorting/Restaurant Customers.cpp
+++ CSES/Searching & Sorting/Restaurant Customers.cpp
@@ -1,12 +1,12 @@
 #include <bits/stdc++.h>
 #define ll long long
 using namespace std;
-
+ 
 int main(){
     ios_base::sync_with_stdio(0);
 	cin.tie(0); cout.tie(0);
     
-    ll n, count=0;
+    ll n, count=0, ans=0;
     
     cin >> n;
     vector< pair<int, int> > t;
@@ -15,18 +15,20 @@ int main(){
         ll x, y;
         cin >> x >> y;
         
-        t.push_back(make_pair(x, y));
+        t.push_back(make_pair(x, 1));
+        t.push_back(make_pair(y, -1));
     }
     
     sort(t.begin(), t.end());
-    for(int i=0; i<n-1; i++){
-        if((t[i].second - t[i].first) > (t[i+1].first - t[i].first))count++;
+    for(auto x:t){
+       count += x.second;
+       ans = max(ans, count);
     }
     
         
-    cout << count << "\n";
+    cout << ans << "\n";
     
     
-
+ 
     return 0;
 }
